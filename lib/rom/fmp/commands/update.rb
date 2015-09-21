6c1d60684a69f01b89499fb3cdd14523ad245916
wbr <wbr@mac.com>
2015-09-21 12:55:47 -0700

    Troubleshoot command results.

--- lib/rom/fmp/commands/update.rb
+++ lib/rom/fmp/commands/update.rb
@@ -11,7 +11,23 @@ module ROM
           # Replaces relation collection with updated records.
           # The 'relation.source.update' is to handle loaded relations.
           new_collection=[]
-          relation.each { |tuple| new_collection << relation.source.update(tuple['record_id'], attributes).one }
+          puts "Update#execute SELF:"
+          puts self.inspect
+          puts "Update#execute RELATION:"
+          puts relation.inspect rescue $!
+          puts "Update#execute RELATION SOURCE:"
+          puts relation.source.inspect rescue $!
+          puts "Update#execute SOURCE:"
+          puts source.inspect rescue $!
+          puts "Update#execute SOURCE RELATION:"
+          puts source.relation.inspect rescue $!
+          relation.each do |tuple|
+            new_collection << case
+              when source.respond_to?(:update); source.update(tuple['record_id'], attributes).one
+              when relation.source.respond_to?(:update); relation.source.update(tuple['record_id'], attributes).one
+              when relation.respond_to?(:update); relation.update(tuple['record_id'], attributes).one
+            end
+          end
           relation.collection.replace(new_collection)
           relation
         end
